66e5d5825e325e5fcf016c9549cb40dde7269aa9
Kathy Darling <helgatheviking@gmail.com>
2014-05-13 18:33:28 +0200

    update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,230 +1,188 @@
 module.exports = function(grunt) {
 
-  require('load-grunt-tasks')(grunt);
-	
-  // Project configuration.
-  grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
-	creds: grunt.file.readJSON('gitcreds.json'),
-	uglify: {
-		options: {
-			compress: {
-				global_defs: {
-					"EO_SCRIPT_DEBUG": false
+	// load most all grunt tasks
+	require('load-grunt-tasks')(grunt);
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+    	gitcreds: grunt.file.readJSON('.gitcreds'),
+		uglify: {
+			options: {
+				compress: {
+					global_defs: {
+						"EO_SCRIPT_DEBUG": false
+					},
+					dead_code: true
 				},
-				dead_code: true
+				banner: '/*! <%= pkg.name %> <%= pkg.version %> */\n'
+			},
+			build: {
+				files: [{
+					expand: true, // Enable dynamic expansion.
+					src: ['js/*.js', '!js/*.min.js'], // Actual pattern(s) to match.
+					ext: '.min.js', // Dest filepaths will have this extension.
+				}, ]
+			}
+		},
+		jshint: {
+			options: {
+				reporter: require('jshint-stylish'),
+				globals: {
+					"EO_SCRIPT_DEBUG": false,
 				},
-			banner: '/*! <%= pkg.title %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd HH:MM") %> */\n'
-		},
-		build: {
-			files: [{
-				expand: true,	// Enable dynamic expansion.
-				src: ['js/*.js', '!js/*.min.js'], // Actual pattern(s) to match.
-				ext: '.min.js',   // Dest filepaths will have this extension.
-			}]
-		}
-	},
-	jshint: {
-		options: {
-			reporter: require('jshint-stylish'),
-			globals: {
-				"EO_SCRIPT_DEBUG": false,
+				'-W020': true, //Read only - error when assigning EO_SCRIPT_DEBUG a value.
 			},
-			 '-W099': true, //Mixed spaces and tabs
-			 '-W083': true,//TODO Fix functions within loop
-			 '-W082': true, //Todo Function declarations should not be placed in blocks
-			 '-W020': true, //Read only - error when assigning EO_SCRIPT_DEBUG a value.
+			all: ['js/*.js', '!js/*.min.js']
 		},
-		all: [ 'js/*.js', '!js/*.min.js' ]
-  	},
 
-	compress: {
-		//Compress build/<%= pkg.name %>
-		main: {
-			options: {
-				mode: 'zip',
-				archive: './deploy/<%= pkg.name %>.zip'
+		// Remove the build directory files
+		clean: {
+			main: ['build/**']
+		},
+
+		// Copy the plugin into the build directory
+		copy: {
+			main: {
+				src: [
+					'**',
+					'!node_modules/**',
+					'!build/**',
+					'!.git/**',
+					'!Gruntfile.js',
+					'!package.json',
+          			'!gitcreds.json',
+          			'!.transifexrc',
+					'!.gitignore',
+					'!.gitmodules',
+					'!**/*.sublime-workspace',
+					'!**/*.sublime-project',
+					'!deploy.sh',
+					'!**/*~'
+				],
+				dest: 'build/'
+			}
+		},
+
+		// Generate git readme from readme.txt
+		wp_readme_to_markdown: {
+			convert: {
+				files: {
+					'readme.md': 'readme.txt'
+				},
 			},
-			expand: true,
-			cwd: 'deploy/<%= pkg.name %>/',
-			src: ['**/*'],
-			dest: '<%= pkg.name %>/'
 		},
-		version: {
-			options: {
-				mode: 'zip',
-				archive: './deploy/<%= pkg.name %>-<%= pkg.version %>.zip'
-			},
-			expand: true,
-			cwd: 'deploy/<%= pkg.name %>/',
-			src: ['**/*'],
-			dest: '<%= pkg.name %>/'
-		}	
-	},
 
-	clean: {
-		//Clean up build folder
-		main: ['deploy/<%= pkg.name %>']
-	},
+		// # Internationalization 
 
-	copy: {
-		// Copy the plugin to a versioned release directory
-		main: {
-			src:  [
-				'**',
-				'!node_modules/**',
-				'!deploy/**',
-				'!.git/**',
-				'!Gruntfile.js',
-				'!package.json',
-				'!gitcreds.json',
-				'!.gitignore',
-				'!.gitmodules',
-				'!*~',
-				'!*.sublime-workspace',
-				'!*.sublime-project',
-				'!*.transifexrc',
-				'!deploy.sh',
-				'!languages/.tx',
-				'!languages/tx.exe'
-			],
-			dest: 'deploy/<%= pkg.name %>/'
+		// Add text domain
+		addtextdomain: {
+			textdomain: '<%= pkg.name %>',
+			target: {
+				files: {
+					src: ['*.php', '**/*.php', '!node_modules/**', '!build/**']
+				}
+			}
 		},
-	
-	},
 
-	wp_readme_to_markdown: {
-		convert:{
+		// Generate .pot file
+		makepot: {
+			target: {
+				options: {
+					domainPath: '/languages', // Where to save the POT file.
+					exclude: ['build'], // List of files or directories to ignore.
+					mainFile: '<%= pkg.name %>.php', // Main project file.
+					potFilename: '<%= pkg.name %>.pot', // Name of the POT file.
+					type: 'wp-plugin' // Type of project (wp-plugin or wp-theme).
+				}
+			}
+		},
+
+		transifex: {
+			'nav-menu-roles': {
+				options: {
+					targetDir: "languages",     
+					mode: "file",
+					filename : "_resource_-_lang_.po",
+				}
+			}
+		},
+
+		// Create .mo files for existing .po
+		po2mo: {
 			files: {
-				'readme.md': 'readme.txt'
+				src: 'languages/*.po',
+				expand: true,
 			},
 		},
-	},
 
-	// # Internationalization 
-
-	// Add text domain
-	addtextdomain: {
-		textdomain: '<%= pkg.name %>',
-		target: {
-			files: {
-				src: ['*.php', '**/*.php', '!node_modules/**', '!deploy/**']
+		// # Check some git repo settings
+		checkrepo: {
+			deploy: {
+				tag: {
+					eq: '<%= pkg.version %>', // Check if highest repo tag is equal to pkg.version
+				},
+				tagged: false, // Check if last repo commit (HEAD) is not tagged
+				clean: true, // Check if the repo working directory is clean
 			}
-		}
-	},
-
-	// Generate .pot file
-	makepot: {
-		target: {
-			options: {
-				domainPath: '/languages', // Where to save the POT file.
-				exclude: ['deploy'], // List of files or directories to ignore.
-				mainFile: '<%= pkg.name %>.php', // Main project file.
-				potFilename: '<%= pkg.name %>.pot', // Name of the POT file.
-				type: 'wp-plugin' // Type of project (wp-plugin or wp-theme).
-			}
-		}
-	},
-
-	// get transifex translations
-	transifex: {
-		"nav-menu-roles": {
-			options: {
-				targetDir: "languages",		// download specified resources / langs only
-		//		resources: ["localizable_enstrings"],
-				languages: ["es"],
-				filename : "<%= pkg.name %>-_lang_.json",
-		//		templateFn: function(strings) { return "bacon"; }
-			}
-		}
-	},
-
-	shell: {
-		options: {
-			stdout: true,
-			stderr: true
 		},
-		txpull: {
-			command: [
-				'cd languages',
-				'tx.exe pull -a -f',
-			].join( '&&' )
-		}
-	},
 
-	// turn po files into mo files
-	po2mo: {
-		files: {
-			src: 'languages/*.po',
-			expand: true,
+    	// # Check the plugin, package & readme files have same version 
+		checkwpversion: {
+			plugin_equals_stable: {
+				version1: 'plugin',
+				version2: 'readme',
+				compare: '==',
+			},
+			plugin_equals_package: {
+				version1: 'plugin',
+				version2: '<%= pkg.version %>',
+				compare: '==',
+			},
 		},
-	},
 
-	// make sure the repo is ok
-	checkrepo: {
-		deploy: {
-			tag: {
-				eq: '<%= pkg.version %>',	// Check if highest repo tag is equal to pkg.version
-			},
-			tagged: true, // Check if last repo commit (HEAD) is not tagged
-			clean: true,   // Check if the repo working directory is clean
-		}
-	},
-
-	// automatically update the docs, scripts on change	
-	watch: {
-		readme: {
-			files: ['readme.txt'],
-			tasks: ['wp_readme_to_markdown'],
+		// # version bump, commit, tag & push in git
+		release: {
 			options: {
-			spawn: false,
-			},
-		  },
-		scripts: {
-			files: ['js/*.js'],
-			tasks: ['newer:jshint','newer:uglify'],
-			options: {
-			spawn: false,
-			},
-		  },
-	},
-
-	// bump version numbers and push tag to github
-	release: {
-		options: {
-			github: { 
-				repo: '<%= pkg.author %>/<%= pkg.name %>', //put your user/repo here
-				usernameVar: '<%= creds.username %>', //ENVIRONMENT VARIABLE that contains Github username 
-				passwordVar: '<%= creds.password %>' //ENVIRONMENT VARIABLE that contains Github password
+				push: true,
+				github: {
+					repo: '<%= pkg.repository.url %>', //put your user/repo here
+					usernameVar: '<%= gitcreds.username %>', //ENVIRONMENT VARIABLE that contains Github username 
+					passwordVar: '<%= gitcreds.password %>' //ENVIRONMENT VARIABLE that contains Github password
+				}
 			}
-		}
-	},
+		},
 
-	// deploy to wordpress.org
-	wp_deploy: {
-		deploy:{
-			options: {
-				svn_user: '<%= pkg.author %>',
-				plugin_slug: '<%= pkg.name %>',
-				build_dir: 'deploy/<%= pkg.name %>/'
-			},
-		}
-	}
+		// # Deploy to WordPress
 
+		wp_deploy: {
+			deploy: {
+				options: {
+					svn_user: '<%= pkg.author %>',
+					plugin_slug: '<%= pkg.name %>',
+					build_dir: 'build/'
+				},
+			}
+		},
 
-});
+	});
 
-grunt.registerTask( 'docs', [ 'wp_readme_to_markdown'] );
+	// makepot and addtextdomain tasks
+	grunt.loadNpmTasks('grunt-wp-i18n');
 
-grunt.registerTask( 'test', [ 'jshint', 'newer:uglify' ] );
+	// Default task(s).
+	grunt.registerTask('default', ['jshint', 'uglify']);
 
-grunt.registerTask( 'build', [ 'test', 'newer:uglify', 'makepot', 'shell:txpull', 'newer:po2mo', 'wp_readme_to_markdown', 'clean', 'copy' ] );
+	grunt.registerTask('test', ['jshint', 'addtextdomain']);
+
+	grunt.registerTask('build', ['test', 'newer:uglify', 'makepot', 'newer:po2mo', 'wp_readme_to_markdown', 'clean', 'copy']);
+
 
 // bump version numbers 
 // grunt release		1.4.1 -> 1.4.2
 // grunt release:minor	1.4.1 -> 1.5.0
 // grint release:major	1.4.1 -> 2.0.0
 
-grunt.registerTask( 'deploy', [ 'checkbranch:master', 'checkrepo:deploy', 'build', 'wp_deploy',  'compress' ] );
+	grunt.registerTask('deploy', ['checkbranch:master', 'checkrepo:deploy', 'build', 'release', 'wp_deploy', 'clean']);
 
 };
