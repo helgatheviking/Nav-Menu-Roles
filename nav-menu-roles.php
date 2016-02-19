d385b81f91efd4d008f468f8275ea1210a6cd1cd
Kathy Darling <Kathy Darling>
2016-02-18 19:07:28 -0500

    revert priority of walker filter. sadface.

--- nav-menu-roles.php
+++ nav-menu-roles.php
@@ -109,7 +109,7 @@ class Nav_Menu_Roles {
 		add_filter( 'plugin_row_meta', array( $this, 'add_action_links' ), 10, 4 );
 
 		// switch the admin walker
-		add_filter( 'wp_edit_nav_menu_walker', array( $this, 'edit_nav_menu_walker' ), 20 );
+		add_filter( 'wp_edit_nav_menu_walker', array( $this, 'edit_nav_menu_walker' ) );
 
 		// add new fields via hook
 		add_action( 'wp_nav_menu_item_custom_fields', array( $this, 'custom_fields' ), 10, 4 );
