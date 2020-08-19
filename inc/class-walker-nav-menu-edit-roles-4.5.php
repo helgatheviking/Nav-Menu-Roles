2597e72a2266ef375465d8475e21ef63b28749a8
Kathy Daring <helgatheviking@gmail.com>
2020-08-19 12:59:14 -0600

    phpcs --snifss=Generic.ControlStructures.InlineControlStructure

--- inc/class-walker-nav-menu-edit-roles-4.5.php
+++ inc/class-walker-nav-menu-edit-roles-4.5.php
@@ -72,8 +72,9 @@ class Walker_Nav_Menu_Edit_Roles extends Walker_Nav_Menu {
 		$original_title = '';
 		if ( 'taxonomy' == $item->type ) {
 			$original_title = get_term_field( 'name', $item->object_id, $item->object, 'raw' );
-			if ( is_wp_error( $original_title ) )
+			if ( is_wp_error( $original_title ) ) {
 				$original_title = false;
+            }
 		} elseif ( 'post_type' == $item->type ) {
 			$original_object = get_post( $item->object_id );
 			$original_title = get_the_title( $original_object->ID );
@@ -105,8 +106,9 @@ class Walker_Nav_Menu_Edit_Roles extends Walker_Nav_Menu {
 		$title = ( ! isset( $item->label ) || '' == $item->label ) ? $title : $item->label;
 
 		$submenu_text = '';
-		if ( 0 == $depth )
+		if ( 0 == $depth ) {
 			$submenu_text = 'style="display: none;"';
+        }
 
 		?>
 		<li id="menu-item-<?php echo $item_id; ?>" class="<?php echo implode( ' ', $classes ); ?>">
