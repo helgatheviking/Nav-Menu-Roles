13580e3b3319b622293e676a7fbf4c413b76e81b
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-02-10 11:37:53 -0500

    Remove compressed/compiled scripts from repo

--- assets/js/customize-controls.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'c00068103bad37edc0d0');
--- assets/js/nav-menu-roles.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '15d5461e1ad4bceb2fc2');
