d21f52c05003cfe489168dac067ebbcb75d6d392
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-02-10 11:01:31 -0500

    update copyright

--- nav-menu-roles.php
+++ nav-menu-roles.php
@@ -9,7 +9,7 @@
  * License: GPL-3.0
  * Text Domain: nav-menu-roles
  *
- * Copyright 2020 Kathy Darling
+ * Copyright 2024 Kathy Darling
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
